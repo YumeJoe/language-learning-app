add123a44a5f4a3f9f9ea1a726a4f0ac47a5ea3d
jokilk1 <nn423567@gmail.com>
2026-02-22 22:11:33 +0300

    Initial commit

---
+++ language-learning-app/src/main/java/com/example/languagelearningapp/LanguageLearningAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.languagelearningapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageLearningAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanguageLearningAppApplication.class, args);
+    }
+
+}
---
+++ language-learning-app/src/test/java/com/example/languagelearningapp/LanguageLearningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.languagelearningapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageLearningAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
